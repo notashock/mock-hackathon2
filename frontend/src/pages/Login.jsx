b4c4c35712e25744cef6dace37db446f1e179f06
sindhu <sindhu@gamil.com>
2026-04-20 11:49:29 +0530

    Register&Login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); 
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-xl shadow-md w-96"
+      >
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          className="w-full border p-2 mb-3 rounded"
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          className="w-full border p-2 mb-4 rounded"
+        />
+
+        <button className="w-full bg-blue-500 text-white p-2 rounded">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
